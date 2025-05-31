bf4d51ab6b4e61ccf175ae297174b8c997369560
Asmodeus <82769834+drasmodeus666@users.noreply.github.com>
2025-05-31 20:03:41 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
     domains: ["placeholder.svg"],
@@ -26,9 +25,6 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: false,
-  },
-  experimental: {
-    esmExternals: "loose",
   },
   transpilePackages: ["lucide-react"],
 }
